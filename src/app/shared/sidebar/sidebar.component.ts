72c297c55dadb55083cbaa52e410876a8c8222de
jheyson zurita soria <jheysoria20@gmai.com>
2021-10-27 21:40:32 -0400

    fix view top sidebar

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
 import {faAngleRight} from '@fortawesome/free-solid-svg-icons';
 import {itemsSide} from '../data/sidebar-home.data';
 
@@ -7,12 +13,29 @@ import {itemsSide} from '../data/sidebar-home.data';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css'],
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit {
   @Input('view') view: boolean = false;
+  public topPos: number = 0;
+
   public itemsSide = itemsSide;
   public faAngleRight = faAngleRight;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.ngAfterViewInit();
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    setTimeout(() => {
+      this.topPos = (<HTMLInputElement>(
+        document.getElementById('header')
+      )).offsetHeight;
+    }, 100);
+  }
 }
