483389a4c902e22e6d1b0ec20a5c59915d497154
jheyson zurita soria <jheysoria20@gmai.com>
2021-10-27 21:40:16 -0400

    prettier changes

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { faBars, faUpload, faBell, faChartLine, faCommentDots, faSearch } from '@fortawesome/free-solid-svg-icons';
+import {Component, OnInit} from '@angular/core';
+import {
+  faBars,
+  faUpload,
+  faBell,
+  faChartLine,
+  faCommentDots,
+  faSearch,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  public faUpload = faUpload
-  public faBars = faBars
-  public faBell = faBell; 
-  public faChartLine =  faChartLine;
-  public faCommentDots =  faCommentDots
-  public faSearch = faSearch
+  public faUpload = faUpload;
+  public faBars = faBars;
+  public faBell = faBell;
+  public faChartLine = faChartLine;
+  public faCommentDots = faCommentDots;
+  public faSearch = faSearch;
 
   public sidebarView = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
