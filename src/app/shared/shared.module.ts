860d682d423e34246677260872b1bbf6013de296
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-03 21:59:51 -0400

    bar and line chart

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,23 @@ import {HeaderComponent} from './header/header.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {RouterModule} from '@angular/router';
+import {BarChartComponent} from './bar-chart/bar-chart.component';
+import {ChartsModule} from 'ng2-charts';
+import {LineChartComponent} from './line-chart/line-chart.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SidebarComponent],
-  imports: [CommonModule, FontAwesomeModule, RouterModule],
-  exports: [HeaderComponent, SidebarComponent],
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BarChartComponent,
+    LineChartComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule, RouterModule, ChartsModule],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BarChartComponent,
+    LineChartComponent,
+  ],
 })
 export class SharedModule {}
