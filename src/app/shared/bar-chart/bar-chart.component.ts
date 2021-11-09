4e12b59f3a287029fe1f4c49edd3d52edbaf8903
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-08 21:52:23 -0400

    bar chart on dashboard

--- src/app/shared/bar-chart/bar-chart.component.ts
+++ src/app/shared/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ChartDataSets, ChartType, ChartOptions} from 'chart.js';
 import {Label} from 'ng2-charts';
 
@@ -8,50 +8,25 @@ import {Label} from 'ng2-charts';
   styleUrls: ['./bar-chart.component.css'],
 })
 export class BarChartComponent implements OnInit {
+  @Input('title') title: string = '';
+
   barChartOptions: ChartOptions = {
     responsive: true,
+    aspectRatio: 1,
+    maintainAspectRatio: false,
     scales: {xAxes: [{}], yAxes: [{}]},
   };
-  barChartLabels: Label[] = [
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-    '2017',
-    '2018',
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-    '2017',
-    '2018',
-    '2013',
-    '2014',
-    '2015',
-    '2016',
-    '2017',
-    '2018',
-  ];
-  barChartType: ChartType = 'horizontalBar';
-  barChartLegend = true;
+  barChartLabels: Label[] = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = false;
   barChartPlugins = [];
 
   barChartData: ChartDataSets[] = [
     {
-      data: [
-        2500, 5900, 6000, 8100, 8600, 8050, 1200, 2500, 5900, 6000, 8100, 8600,
-        8050, 1200, 2500, 5900, 6000, 8100, 8600, 8050, 1200, 2500, 5900, 6000,
-        8100, 8600, 8050, 1200,
-      ],
-      label: 'Company A',
-    },
-    {
-      data: [
-        2800, 4800, 4000, 7900, 9600, 8870, 1400, 2500, 5900, 6000, 8100, 8600,
-        8050, 1200, 2800, 4800, 4000, 7900, 9600, 8870, 1400, 2500, 5900, 6000,
-        8100, 8600, 8050, 1200,
-      ],
-      label: 'Company B',
+      data: [2500, 5900, 11000, 8100, 8600, 8050],
+      label: 'Visitors',
+      barPercentage: 0.2,
+      backgroundColor: '#6d5cb3',
     },
   ];
   constructor() {}
